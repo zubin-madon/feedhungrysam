b9dbd26a4b41c33f3ffd0bdcd42f8dcda2478348
zubin-madon <madon.zubin@gmail.com>
2023-02-11 10:20:48 +0530

    one pager first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="bg-[url('/jail.jpg')] h-screen w-full bg-cover bg-center bg-no-repeat relative font-mono">
+    <div class="container basis-1/3 w-1/3 text-left absolute right-10 mt-10">
+    <div class="container mb-5">
+    <h1 class="text-6xl text-slate-200 text-bold">FeedHungrySam</h1> 
+    </div>
+    <div class="container mb-5 text-slate-400 text-xl">
+    <p class="mb-10">
+    FEAT is the first on chain derivative of FTXs bankruptcy assets, without risk of liquidation. The FEAT team was so disgustedly coping due to the FTX fraud, that they decided to come up with a way for the cryptoverse to lever up the value of their own bankruptcy assets. <br/>
+    The FTX lawyers are making over $2000/hr. The FTX CEO John Ray is making over a $1000 an hour. There's only 1 way to fight this - by levering up as degenerately as possible.<br/>
+
+FEAT's mission is to increase the price of everyone's bankruptcy assets by acquiring spot FTT & opening levered positions with proceeds from a 2% tax. Toxic leeches like John Ray, Kyle & Zhu, and FTX's legal team are all seeking to drain user funds for their own gain. It's time we created a vehicle for users to fight back.
+
+    </p>
+    <p>
+    The project allocation is as follows:
+    <ul>
+    <li>* 10% team</li>
+    <li>* 10% marketing and development to be used to pay influencers are partnerships (tokens can also be used for liquidity incentives if required)</li>
+    <li>* 5% presale</li>
+    <li>* 75% liquidity</li>
+    </ul>
+    </p>
+    </div>
+    <div class="flex space-x-2 justify-left">
+      <div>
+      <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">One Pager</button>
+      <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">Token</button>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
