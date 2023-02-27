ee7b22db5d246373078bdad7e212131e646acf5e
Zubin Madon <35947376+zubin-madon@users.noreply.github.com>
2023-02-27 15:24:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
+    <div class="bg-black>
     <div class="bg-[url('/jail.jpg')] md:h-screen md:w-full md:bg-cover bg-center bg-no-repeat relative font-mono">
-    <div class="m-10 bg-black container md:basis-1/3 md:w-1/3 md:text-left md:absolute md:right-10 md:mt-10">
+    <div class="m-10 container md:basis-1/3 md:w-1/3 md:text-left md:absolute md:right-10 md:mt-10">
     <div class="container mb-5">
     <h1 class="text-6xl text-slate-200 text-bold">FeedHungrySam</h1> 
     </div>
@@ -18,6 +19,7 @@ FEAT's mission is to increase the price of everyone's bankruptcy assets by acqui
       <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">One Pager</button>
       <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">Token</button>
       </div>
+    </div>
     </div>
     </div>
     </div>
