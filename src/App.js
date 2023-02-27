71a258b43060b73d9259fc9a21516b550e46fe87
Mohsin Ali <108330223+Mohsin191@users.noreply.github.com>
2023-02-27 20:43:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './App.css';
 function App() {
   return (
    <>
-<div class="bg-[url('/jail.jpg')] md:h-screen md:w-full md:bg-cover bg-center bg-no-repeat relative font-mono">
-    <div class="m-10 container md:basis-1/3 md:w-1/3 md:text-left md:absolute md:right-10 md:mt-10">
-    <div class="container mb-5">
-    <h1 class="text-6xl text-slate-200 text-bold">FeedHungrySam</h1> 
-    </div>
-    <div class="container mb-5 text-slate-400 text-xl">
-    <p class="mb-10">
-FEAT's mission is to increase the price of everyone's bankruptcy assets by acquiring spot FTT & opening levered positions with proceeds from a 2% tax. Toxic leeches like John Ray, Kyle & Zhu, and FTX's legal team are all seeking to drain user funds for their own gain. It's time we created a vehicle for users to fight back.
-    </p>
-    </div>
-    <div class="flex space-x-2 justify-left">
-      <div>
-      <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">One Pager</button>
-      <button type="button" class="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">Token</button>
-      </div>
-    </div>
-    </div>
-    </div>
+<div className=' bg-[url(./jail.jpg)] for__bg w-full h-screen md:w-full bg-cover bg-center bg-no-repeat relative font-mono '>
+          <div className=' container mx-auto lg:w-1/3 md:w-1/2 w-full md:text-left absolute md:top-10 sm:top:40  lg:right-20 md:right-10 md:p-0 p-5 top-20' >
+            <div className="container mb-5">
+              <h1 className="md:text-6xl text-4xl text-slate-200 text-bold">FeedHungrySam</h1>
+            </div>
+            <div className="container mb-5 text-slate-400 text-xl">
+              <p className="mb-10">
+                FEAT's mission is to increase the price of everyone's bankruptcy assets by acquiring spot FTT & opening levered positions with proceeds from a 2% tax. Toxic leeches like John Ray, Kyle & Zhu, and FTX's legal team are all seeking to drain user funds for their own gain. It's time we created a vehicle for users to fight back.
+              </p>
+            </div>
+            <div className="flex space-x-2 justify-left">
+              <div>
+                <button type="button" className="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">One Pager</button>
+                <button type="button" className="inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out m-5">Token</button>
+              </div>
+            </div>
+          </div>
+        </div>
    </>
   );
 }
